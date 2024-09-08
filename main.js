c42653a0820bb07594edc371f98d362288aea759
Chetan <227r1a66e1@cmrtc.ac.in>
2024-09-08 22:22:43 +0530

    final changes

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  document.getElementById('hamburger').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-  });
-});
